0d0fbfabf2d5a61ce0ee307a8a4cf31c23298479
dustinmaciasz16-star <dustinmaciasz16@gmail.com>
2026-04-09 20:51:08 -0500

    modificacion del html y el js.

--- cazando copy.js
+++ cazando copy.js
@@ -1,13 +1,13 @@
 let canvas = document.getElementById("pantalla");
 let ctx = canvas.getContext("2d");
-// Gato
+// variables y contantes del Gato
 let gatoX = 0;
 let gatoY = 0;
 const ANCHOGATO = 50;
 const ALTURAGATO = 50;
 const VELOCIDAD = 10;
  
-// Raton
+// variable y constantes de la comida
 let comidaX = 0;
 let comidaY = 0;
 const ANCHOCOMIDA = 30;
